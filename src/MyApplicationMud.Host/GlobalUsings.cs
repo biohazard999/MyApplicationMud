e0c627d534daf80ce2a21219dd66ffd652f0a953
Manuel Grundner <m.grundner@delegate.at>
2022-06-21 11:06:32 +0200

    feat: added bff and hosting via bff host

---
+++ src/MyApplicationMud.Host/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using System.Diagnostics;
+global using System.Security.Claims;
+
+global using MyApplicationMud.Host;
+global using MyApplicationMud.Host.Services;
+global using MyApplicationMud.Shared.Authorization;
+global using MyApplicationMud.Shared.Defaults;
+
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authentication.Cookies;
+global using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
