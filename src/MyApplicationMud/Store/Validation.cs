fa680f0bb41f36354f3af1a2ae65a38aad4c86ae
PS Developer <register@paragraph-software.at>
2022-07-06 11:38:14 +0200

    feat: added validation state

--- src/MyApplicationMud/Store/Validation.cs
+++ src/MyApplicationMud/Store/Validation.cs
@@ -2,4 +2,29 @@
 
 public record ValidationState
 {
+    public bool HasErrors => !Errors.Any();
+    public IList<KeyValuePair<string, string>> Errors { get; init; } = new List<KeyValuePair<string, string>>();
+}
+
+public class ValidationStateFeature : Feature<ValidationState>
+{
+    public override string GetName() => nameof(ValidationState);
+    protected override ValidationState GetInitialState()
+        => new();
+}
+
+[Dispatchable]
+public record ClearValidation();
+[Dispatchable]
+public record SetValidationErrors(IEnumerable<KeyValuePair<string, string>> Errors);
+
+public static class ValidationStateReducers
+{
+    [ReducerMethod(typeof(ClearValidation))]
+    public static ValidationState ClearValidation(ValidationState state)
+        => state with { Errors = new List<KeyValuePair<string, string>>() };
+
+    [ReducerMethod]
+    public static ValidationState SetValidationErrors(ValidationState state, SetValidationErrors action)
+        => state with { Errors = action.Errors.ToList() };
 }
