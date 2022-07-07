4bef51945f0a601154e5713c677b09267bb23567
Manuel Grundner <m.grundner@delegate.at>
2022-07-07 11:52:13 +0200

    feat: basic modular structure

---
+++ src/Client/Modules/MyApplicationMud.AppModule/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApplicationMud.AppModule.MyFeature.Pages;
+public class Page1Model : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ src/Client/Modules/MyApplicationMud.BooksModule/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApplicationMud.BooksModule.MyFeature.Pages;
+public class Page1Model : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
